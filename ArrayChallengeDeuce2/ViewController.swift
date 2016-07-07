a5712662b306aba2f31a94fdb7116849fcd7a51e
Cordavi <michaelamundsen@gmail.com>
2016-07-07 14:26:54 -0400

    Created addNameToDeliLine func so test pass

--- ArrayChallengeDeuce2/ViewController.swift
+++ ArrayChallengeDeuce2/ViewController.swift
@@ -12,16 +12,28 @@ class ViewController: UIViewController {
     
     // This represents our deliLine.
     var deliLine: [String] = []
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-   
     
-    
-    // Create your methods here
-
-
+    func addNameToDeliLine(name:String) -> String {
+        switch name {
+        case "Billy Crystal":
+            deliLine.insert(name, atIndex: 0)
+            return "Welcome Billy! You can sit wherever you like."
+        case "Meg Ryan":
+            deliLine.insert(name, atIndex: 0)
+            return "Welcome Meg! You can sit wherever you like."
+        default:
+            if deliLine.isEmpty {
+                deliLine.append(name)
+                return "Welcome \(name), you're first in line!"
+            } else {
+                deliLine.append(name)
+                let placeIndex = deliLine.indexOf(name)
+                return "Welcome \(name), you're number \(placeIndex! + 1) in line."
+            }
+        }
+    }
 }
-
